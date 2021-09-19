2076286a69e3a78a32739f06a0c7182e19459c09
minseo23 <neverdie4757@gmail.com>
2021-09-19 13:49:35 +0900

    쿼리메소드의 기본실습 1

--- src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
+++ src/main/java/com/fastcampus/jpa/bookmanager/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.fastcampus.jpa.bookmanager.repository;
 
 import com.fastcampus.jpa.bookmanager.domain.User;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -28,4 +30,21 @@ public interface UserRepository extends JpaRepository<User, Long> {
     User findFirst1ByName(String name);
 
     User findTop1ByName(String name);
+
+    List<User> findByEmailAndName(String email, String name);
+
+    List<User> findByEmailOrName(String email, String name);
+
+    List<User> findByCreateAtAfter(LocalDateTime yesterday);
+
+    List<User> findByIdAfter(Long id);
+
+    List<User> findByCreateAtGreaterThan(LocalDateTime yesterday);
+
+    List<User> findByCreateAtGreaterThanEqual(LocalDateTime yesterday);
+
+    List<User> findByCreateAtBetween(LocalDateTime yesterday, LocalDateTime tomorrow);
+
+    List<User> findByIdBetween(Long id1, Long id2);
+
 }
--- src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
+++ src/test/java/com/fastcampus/jpa/bookmanager/repository/UserRepositoryTest.java
@@ -10,6 +10,8 @@ import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.endsWith;
@@ -34,18 +36,26 @@ class UserRepositoryTest {
     void select() {
         System.out.println(userRepository.findByName("david"));
 
-        userRepository.save(new User("david", "david@naver.com"));
+        userRepository.save(new User(1L,"david", "david@naver.com", LocalDateTime.now(), LocalDateTime.now()));
 
-        System.out.println("findByEmail = " + userRepository.findByEmail("david@naver.com"));
-        System.out.println("getByEmail = " + userRepository.getByEmail("david@naver.com"));
-        System.out.println("readByEmail = " + userRepository.readByEmail("david@naver.com"));
-        System.out.println("queryByEmail = " + userRepository.queryByEmail("david@naver.com"));
-        System.out.println("searchByEmail = " + userRepository.searchByEmail("david@naver.com"));
-        System.out.println("streamByEmail = " + userRepository.streamByEmail("david@naver.com"));
-        System.out.println("findUserByEmail = " + userRepository.findUserByEmail("david@naver.com"));
-        System.out.println("findSomethingByEmail = " + userRepository.findSomethingByEmail("david@naver.com"));
-        System.out.println("findFirstByName = " + userRepository.findFirst1ByName("david"));
-        System.out.println("findTop1ByName = " + userRepository.findTop1ByName("david"));
+//        System.out.println("findByEmail = " + userRepository.findByEmail("david@naver.com"));
+//        System.out.println("getByEmail = " + userRepository.getByEmail("david@naver.com"));
+//        System.out.println("readByEmail = " + userRepository.readByEmail("david@naver.com"));
+//        System.out.println("queryByEmail = " + userRepository.queryByEmail("david@naver.com"));
+//        System.out.println("searchByEmail = " + userRepository.searchByEmail("david@naver.com"));
+//        System.out.println("streamByEmail = " + userRepository.streamByEmail("david@naver.com"));
+//        System.out.println("findUserByEmail = " + userRepository.findUserByEmail("david@naver.com"));
+//        System.out.println("findSomethingByEmail = " + userRepository.findSomethingByEmail("david@naver.com"));
+//        System.out.println("findFirstByName = " + userRepository.findFirst1ByName("david"));
+//        System.out.println("findTop1ByName = " + userRepository.findTop1ByName("david"));
+        System.out.println("findByEmailAndName = " + userRepository.findByEmailAndName("david@naver.com", "david"));
+        System.out.println("findByEmailOrName = " + userRepository.findByEmailOrName("david@naver.com", "david"));
+        System.out.println("findByCreateAtAfter = " + userRepository.findByCreateAtAfter(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByIdAfter = " + userRepository.findByIdAfter(1L));
+        System.out.println("findByCreateAtGreaterThan = " + userRepository.findByCreateAtGreaterThan(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByCreateAtGreaterThanEqual = " + userRepository.findByCreateAtGreaterThanEqual(LocalDateTime.now().minusDays(1L)));
+        System.out.println("findByCreateAtBetween = " + userRepository.findByCreateAtBetween(LocalDateTime.now().minusDays(1L), LocalDateTime.now().plusDays(1L)));
+        System.out.println("findByIdBetween = " + userRepository.findByIdBetween(1L, 2L));
 
     }
 }
